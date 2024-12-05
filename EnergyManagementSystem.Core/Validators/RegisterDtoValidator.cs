73c60a77ba8c0b8865e425eedf00d58830e9dd92
Alper Torun <alper.torun@piworks.net>
2024-12-05 18:22:07 +0300

    feat: adds Validators

---
+++ EnergyManagementSystem.Core/Validators/RegisterDtoValidator.cs
@@ -0,0 +1,30 @@
+﻿using FluentValidation;
+using EnergyManagementSystem.Core.DTOs.Auth;
+
+namespace EnergyManagementSystem.Core.Validators
+{
+    public class RegisterDtoValidator : AbstractValidator<RegisterDto>
+    {
+        public RegisterDtoValidator()
+        {
+            RuleFor(x => x.Email)
+                .NotEmpty().WithMessage("Email adresi zorunludur.")
+                .EmailAddress().WithMessage("Geçerli bir email adresi giriniz.");
+
+            RuleFor(x => x.Password)
+                .NotEmpty().WithMessage("Şifre zorunludur.")
+                .MinimumLength(6).WithMessage("Şifre en az 6 karakter olmalıdır.")
+                .Matches("[A-Z]").WithMessage("Şifre en az bir büyük harf içermelidir.")
+                .Matches("[a-z]").WithMessage("Şifre en az bir küçük harf içermelidir.")
+                .Matches("[0-9]").WithMessage("Şifre en az bir rakam içermelidir.");
+
+            RuleFor(x => x.Name)
+                .NotEmpty().WithMessage("İsim zorunludur.")
+                .MinimumLength(2).WithMessage("İsim en az 2 karakter olmalıdır.")
+                .MaximumLength(50).WithMessage("İsim en fazla 50 karakter olabilir.");
+
+            RuleFor(x => x.ConfirmPassword)
+                .Equal(x => x.Password).WithMessage("Şifreler eşleşmiyor.");
+        }
+    }
+}
