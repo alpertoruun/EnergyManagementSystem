fe89e69d603073eb2b9b95306b8ffe63697be7a7
Alper Torun <71210135+alpertoruun@users.noreply.github.com>
2025-01-07 12:27:08 +0300

    feat: removes credentials

--- EnergyManagementSystem.Core/Configuration/EmailSettings.cs
+++ EnergyManagementSystem.Core/Configuration/EmailSettings.cs
@@ -6,13 +6,14 @@ using System.Threading.Tasks;
 
 namespace EnergyManagementSystem.Core.Configuration
 {
+    // Your configuration
     public class EmailSettings
     {
-        public string Host { get; set; } = "smtp.gmail.com";
+        public string Host { get; set; } = "smtp.gmail.com"; 
         public int Port { get; set; } = 587;
         public bool UseTls { get; set; } = true;
-        public string Username { get; set; } = "alpertorun4455@gmail.com";
-        public string Password { get; set; } = "weew audi svvu mbio";
+        public string Username { get; set; } = "xxxx";
+        public string Password { get; set; } = "xxx";
         public string ClientUrl { get; set; } = "http://localhost:5173";
         public string ApiUrl { get; set; } = "http://localhost:5286";
     }
